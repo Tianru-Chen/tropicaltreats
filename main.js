8581873709e82c8a442bda6b2ee21ea212e8b61d
Tianru Chen <tianruchen@Tianrus-MacBook-Pro.local>
2019-08-28 23:11:42 +1000

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+function open_nav_side() {
+    document.getElementById("navigation-side").style.display = "block";
+}
+
+function close_nav_side() {
+    document.getElementById("navigation-side").style.display = "none";
+}
