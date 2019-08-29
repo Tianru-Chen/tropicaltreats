6c4e502ca555ca2c727071cde8486a3b43cf8ef0
Tianru Chen <tianruchen@Tianrus-MacBook-Pro.local>
2019-08-30 06:45:22 +1000

    remove unused file

--- main.js
+++
@@ -1,7 +0,0 @@
-function open_nav_side() {
-    document.getElementById("navigation-side").style.display = "block";
-}
-
-function close_nav_side() {
-    document.getElementById("navigation-side").style.display = "none";
-}
